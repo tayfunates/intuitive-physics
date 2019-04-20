cd7285bcd0bdf5dc00146163cc6ac9b89fbcd02d
Tayfun Ateş <tayfun@pixerylabs.com>
2019-04-20 13:58:48 +0300

    Able to change the resolution of the scene.

---
+++ simulation/what-if/Assets/Scripts/PhysicsSimulationsBase.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PhysicsSimulationsBase : MonoBehaviour
+{
+    private Camera cam;
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected void stop()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+
+    protected IEnumerator captureScreenshot(string path)
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D screenImage = new Texture2D(Screen.width, Screen.height);
+        //Get Image from screen
+        screenImage.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenImage.Apply();
+        //Convert to png
+        byte[] imageBytes = screenImage.EncodeToPNG();
+
+        //Save image to file
+        System.IO.File.WriteAllBytes(path, imageBytes);
+
+    }
+}

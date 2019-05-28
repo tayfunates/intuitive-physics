a9ff118026225fcf18469671bc5d8dd2f9e29b93
Tayfun Ateş <tayfun@pixerylabs.com>
2019-05-28 14:06:22 +0300

    Id segmentation of the generated images.

--- simulation/test/gym_test.py
+++ simulation/test/gym_test.py
@@ -6,6 +6,12 @@ import subprocess
 import os
 import ast
 import random
+import matplotlib
+matplotlib.use('PS')
+import pylab as plt
+import matplotlib.image as mpimg
+import numpy as np
+from PIL import Image
 
 class SimulationState(Enum):
     CREATE_SCENE = 0
@@ -42,6 +48,31 @@ def readInitialStableConfigurationObjectCount(filePath):
 
     sceneDict = ast.literal_eval(sceneJSONStr)
     return len(sceneDict['objectStates'])
+
+
+def convertSegmentationImageColorsToIndices(img, noObjects):
+    thresholds = []
+    for i in range(1, noObjects+1):
+        thresholds.append(int(i * 255.0 / noObjects))
+
+    ret = np.zeros([img.shape[0], img.shape[1], 3], dtype=np.uint8)
+
+    threshConfidence = 3
+    for i in range(0, len(thresholds)):
+        lower = thresholds[i]-threshConfidence
+        upper = thresholds[i]+threshConfidence
+        mask = ((img >= [lower, lower, lower]) & (img <= [upper, upper, upper])).all(axis=2)
+
+        id = i+1
+
+        ret[mask] = [id, id, id]
+
+    return ret
+
+def convertSegmentationImageIndicesToColors(img, noObjects):
+    inc = int(255.0 / noObjects)
+    ret = inc * img
+    return ret
 
 
 simulation_count = 100
@@ -83,6 +114,14 @@ for i in range(simulation_count):
         p1 = subprocess.Popen([unity_call_str], shell=True, stdout=subprocess.PIPE)
         p1.wait()
 
+        segmentationImageNameVisible = os.path.join(simulationFolder, (os.path.splitext(initial_stable_json_path)[0]+"_VisibleSeg.png"))
+        initialSegmentationImageVisible = np.uint8(mpimg.imread(segmentationImageNameVisible) * 255)
+
+        initialSegmentationImageNONVisible = convertSegmentationImageColorsToIndices(initialSegmentationImageVisible, noObjects)
+        segmentationImageNameNONVisible = os.path.join(simulationFolder, (os.path.splitext(initial_stable_json_path)[0]+"_NONVisibleSeg.png"))
+        im = Image.fromarray(initialSegmentationImageNONVisible)
+        im.save(segmentationImageNameNONVisible)
+
         #Start creating final stable configurations
         if (os.path.exists(simulationFolder)):
             for j in range(noObjects):
@@ -103,6 +142,14 @@ for i in range(simulation_count):
 
                 p2 = subprocess.Popen([unity_call_str], shell=True, stdout=subprocess.PIPE)
                 p2.wait()
+
+                segmentationImageNameVisible = os.path.join(simulationFolder, (os.path.splitext(SimulationControllerState['inputSceneJSON'])[0] + "_VisibleSeg.png"))
+                finalSegmentationImageVisible = np.uint8(mpimg.imread(segmentationImageNameVisible) * 255)
+
+                finalSegmentationImageNONVisible = convertSegmentationImageColorsToIndices(finalSegmentationImageVisible, noObjects)
+                segmentationImageNameNONVisible = os.path.join(simulationFolder, (os.path.splitext(SimulationControllerState['inputSceneJSON'])[0] + "_NONVisibleSeg.png"))
+                im = Image.fromarray(finalSegmentationImageNONVisible)
+                im.save(segmentationImageNameNONVisible)
 
 
 

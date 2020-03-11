0e0c9507bcec47b6542512d6e21cd8047621ec40
M. Samil Atesoglu <m.samilatesoglu@gmail.com>
2020-03-11 19:31:43 +0300

    Change key literals with constants.

--- dataset/src/event.py
+++ dataset/src/event.py
@@ -2,8 +2,14 @@
 
 class Event:
 
+    KEY_ID = "id"
+    KEY_OBJECTS = "objects"
+    KEY_STEP = "step"
+    KEY_TYPE = "type"
+
     def __init__(self, event_dict):
-        self.id = event_dict["id"]
-        self.objects = event_dict["objects"]
-        self.step = event_dict["step"]
-        self.type = event_dict["type"]
+        self.id = event_dict[Event.KEY_ID]
+        self.objects = event_dict[Event.KEY_OBJECTS]
+        self.step = event_dict[Event.KEY_STEP]
+        self.type = event_dict[Event.KEY_TYPE]
+

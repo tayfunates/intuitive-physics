27fb303c3d730f1170ea78657e1d9fa7cbb04e00
Tayfun Ateş <tayfun.ates@avcrtech.com>
2020-03-11 21:42:17 +0300

    enum fixes.

--- dataset/svqa/object.py
+++ dataset/svqa/object.py
@@ -3,36 +3,36 @@ from enum import Enum
 class Object:
 
     class ColorType(Enum):
-        BLUE = "blue",
-        BROWN = "brown",
-        CYAN = "cyan",
-        GRAY = "gray",
-        GREEN = "green",
-        PURPLE = "purple",
-        RED = "red",
-        YELLOW = "yellow",
+        BLUE = "blue"
+        BROWN = "brown"
+        CYAN = "cyan"
+        GRAY = "gray"
+        GREEN = "green"
+        PURPLE = "purple"
+        RED = "red"
+        YELLOW = "yellow"
         BLACK = "black"
 
     class GeometryType(Enum):
-        SMALL_CUBE = "s_cube",
-        BIG_CUBE = "b_cube",
-        STANDARD_RECTANGLE = "std_rect",
-        SMALL_TRIANGLE = "s_tri",
-        BIG_TRIANGLE = "b_tri",
-        SMALL_HEXAGON = "s_hex",
-        BIG_HEXAGON = "b_hex",
-        WALL_PIN = "pin",
-        ROPE_UNIT = "rp_unit",
-        SMALL_CIRCLE = "s_circle",
-        BIG_CIRCLE = "b_circle",
-        BIG_RAMP = "b_ramp",
-        CUSTOM_RECTANGLE = "cus_rectange",
-        CAR_BODY = "car_bd",
-        CAR_WHEEL = "car_wh",
-        ROD_RECTANGLE = "rod",
-        BASKET = "basket",
-        LEFT_BOUNDARY = "l_bound",
-        RIGHT_BOUNDARY = "r_bound",
+        SMALL_CUBE = "s_cube"
+        BIG_CUBE = "b_cube"
+        STANDARD_RECTANGLE = "std_rect"
+        SMALL_TRIANGLE = "s_tri"
+        BIG_TRIANGLE = "b_tri"
+        SMALL_HEXAGON = "s_hex"
+        BIG_HEXAGON = "b_hex"
+        WALL_PIN = "pin"
+        ROPE_UNIT = "rp_unit"
+        SMALL_CIRCLE = "s_circle"
+        BIG_CIRCLE = "b_circle"
+        BIG_RAMP = "b_ramp"
+        CUSTOM_RECTANGLE = "cus_rectange"
+        CAR_BODY = "car_bd"
+        CAR_WHEEL = "car_wh"
+        ROD_RECTANGLE = "rod"
+        BASKET = "basket"
+        LEFT_BOUNDARY = "l_bound"
+        RIGHT_BOUNDARY = "r_bound"
         BOTTOM_BOUNDARY = "b_bound"
 
 

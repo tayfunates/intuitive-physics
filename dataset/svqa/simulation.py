5bd67a40999a23ea20d849e49c01aa051fc6a266
Tayfun Ateş <tayfun.ates@avcrtech.com>
2020-03-11 21:32:46 +0300

    fixes.

--- dataset/svqa/simulation.py
+++ dataset/svqa/simulation.py
@@ -1,10 +1,10 @@
-from src.object import Object
+from svqa.object import Object
 
 class Simulation:
-    def __init__(self, initial_scene_dict, end_scene_dict, causal_graph_dict ):
+    def __init__(self, start_scene_dict, end_scene_dict, causal_graph_dict):
         self.objects = []
-        for object_state_at_init, object_state_at_end in zip(initial_scene_dict, end_scene_dict):
-            assert object_state_at_init[Object.KEY_UNIQUE_ID] == object_state_at_end[Object.KEY_UNIQUE_ID]
-            self.objects.append(Object(object_state_at_init, object_state_at_end))
+        for object_state_at_start, object_state_at_end in zip(start_scene_dict, end_scene_dict):
+            assert object_state_at_start[Object.KEY_UNIQUE_ID] == object_state_at_end[Object.KEY_UNIQUE_ID]
+            self.objects.append(Object(object_state_at_start, object_state_at_end))
 
         self.causal_graph = causal_graph_dict

8859a8d6cb1b2c089e06f1bc2e4b4c16f4a09b0e
Tayfun Ateş <tayfun.ates@avcrtech.com>
2020-03-11 21:30:01 +0300

    added some tests.

--- dataset/src/simulation.py
+++ dataset/src/simulation.py
@@ -1,10 +1,10 @@
-from object import Object
+from src.object import Object
 
 class Simulation:
     def __init__(self, initial_scene_dict, end_scene_dict, causal_graph_dict ):
         self.objects = []
         for object_state_at_init, object_state_at_end in zip(initial_scene_dict, end_scene_dict):
-            assert object_state_at_init[Object.KEY_UNIQUE_ID] != object_state_at_end[Object.KEY_UNIQUE_ID]
+            assert object_state_at_init[Object.KEY_UNIQUE_ID] == object_state_at_end[Object.KEY_UNIQUE_ID]
             self.objects.append(Object(object_state_at_init, object_state_at_end))
 
         self.causal_graph = causal_graph_dict
--- dataset/test/test.py
+++ dataset/test/test.py
@@ -1,5 +1,6 @@
 import json
 
+from src.simulation import Simulation
 from src.causal_graph import CausalGraph
 from src.event import Event
 
@@ -16,11 +17,29 @@ def test__event_class():
 
 
 def test__causal_graph():
-    with open("../graph.json") as graph_file:
+    with open("../../simulation/2d/SVQA-Box2D/Testbed/graph.json") as graph_file:
         string = graph_file.read()
         causal_graph = CausalGraph(json.loads(string))
+
+def test__json_read():
+    initial_scene_path = "../../simulation/2d/SVQA-Box2D/Testbed/scene.json"
+    end_scene_path = "../../simulation/2d/SVQA-Box2D/Testbed/scene.json"
+    causal_graph_path = "../../simulation/2d/SVQA-Box2D/Testbed/graph.json"
+
+    with open(initial_scene_path) as json_file:
+        initial_scene = json.load(json_file)
+
+    with open(end_scene_path) as json_file:
+        end_scene = json.load(json_file)
+
+    with open(causal_graph_path) as json_file:
+        causal_graph = json.load(json_file)
+
+    sim = Simulation(initial_scene, end_scene, causal_graph)
 
 
 if __name__ == "__main__":
     assert test__event_class()
     test__causal_graph()
+
+    test__json_read()

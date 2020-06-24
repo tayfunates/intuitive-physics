cfab45c4d96ca9aa7274f7d8eeff4393917155b7
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-06-24 22:32:31 +0300

    Load dataset when clicked.

--- dataset/gui/dataset_viewer.py
+++ dataset/gui/dataset_viewer.py
@@ -1,23 +1,45 @@
+import json
 import sys
 from PyQt5 import QtWidgets, uic
 
+
 # Maybe create a dataset viewer GUI to inspect data comfortably?
+
+
+class Dataset:
+
+    def __init__(self, dataset_json):
+        self.dataset = dataset_json
+
+
+g_dataset = None
 
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()  # Call the inherited classes __init__ method
         uic.loadUi('dataset_viewer.ui', self)  # Load the .ui file
+
         self.initialize_components()
+        self.initialize_listeners()
+
         self.show()  # Show the GUI
+
+    def initialize_listeners(self):
+        self.btn_load.clicked.connect(self.load_dataset)
 
     def initialize_components(self):
         # To do...
-        self.load_button = self.findChild(QtWidgets.QPushButton, "loadDatasetButton")
-        self.load_button.clicked.connect(self.load_dataset_button_clicked)
+        self.btn_load = self.findChild(QtWidgets.QPushButton, "loadDatasetButton")
+        self.le_dataset_folder = self.findChild(QtWidgets.QLineEdit, "datasetFolderLineEdit")
 
-    def load_dataset_button_clicked(self):
+    def load_dataset(self):
         print("Loading dataset...")
+        path = self.le_dataset_folder.text()
+        with open(f"{path}/dataset.json", "r") as json_file:
+            global g_dataset
+            g_dataset = Dataset(json.load(json_file))
+        print(f"Dataset at {path} loaded...")
 
 
 if __name__ == '__main__':

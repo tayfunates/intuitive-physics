0851304d975267b6666956d70b8ac05211befcad
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-07-30 00:14:33 +0300

    Implement strict undersampling method for uniformity.
    #48.

--- dataset/autorun/dataset_utils.py
+++ dataset/autorun/dataset_utils.py
@@ -1,6 +1,7 @@
 import copy
 import json
 import pathlib
+import pandas as pd
 
 
 def minimized_dataset(dataset_json) -> dict:
@@ -21,6 +22,30 @@ def minimized_dataset(dataset_json) -> dict:
 def relativize_paths(dataset_json, dataset_folder_path) -> dict:
     folder_name = pathlib.Path(dataset_folder_path).name
     return json.loads(json.dumps(dataset_json).replace(dataset_folder_path, f"./{folder_name}"))
+
+
+def balance_dataset(dataset_json) -> dict:
+    new_dataset = []
+    for qa_json in dataset_json:
+        question_list = qa_json["questions"]["questions"]
+        q_list_json = undersample_data(question_list, "answer")
+        qa_json["questions"]["questions"] = q_list_json
+        new_dataset.append(qa_json)
+
+    return json.loads(json.dumps(new_dataset))
+
+
+def undersample_data(data, class_name: str):
+    """
+    Strictly performs undersampling by randomly deleting excess elements.
+    :param data: List of data with features.
+    :param class_name: Name of the feature that will be used in grouping.
+    :return:
+    """
+    df = pd.DataFrame(data)
+    g = df.groupby(class_name)
+    df = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))
+    return json.loads(df.to_json(orient='records'))
 
 
 # TODO: Move statistics computation here.

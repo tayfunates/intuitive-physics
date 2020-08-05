2668a9727b3ebe34ff38ea67303d90a140e862f0
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-08-05 22:17:47 +0300

    Helper functions for grouping dataset.

--- dataset/autorun/dataset_utils.py
+++ dataset/autorun/dataset_utils.py
@@ -24,6 +24,53 @@ def relativize_paths(dataset_json, dataset_folder_path) -> dict:
     return json.loads(json.dumps(dataset_json).replace(dataset_folder_path, f"./{folder_name}"))
 
 
+def dataset_grouped_by_templates(dataset_json) -> dict:
+    templates = defaultdict(list)
+
+    for qa_json in dataset_json:
+        question_list = qa_json["questions"]["questions"]
+        for question_obj in question_list:
+            template_filename = question_obj["template_filename"]
+            answer = question_obj["answer"]
+            question = question_obj["question"]
+            video_file_path = question_obj["video_filename"]
+            templates[template_filename].append({"question": question,
+                                                 "answer": answer,
+                                                 "video_file_path": video_file_path})
+    return templates
+
+
+def dataset_grouped_by_video_indices(dataset_json) -> dict:
+    videos = defaultdict(list)
+
+    for qa_json in dataset_json:
+        question_list = qa_json["questions"]["questions"]
+        for question_obj in question_list:
+            template_filename = question_obj["template_filename"]
+            answer = question_obj["answer"]
+            question = question_obj["question"]
+            video_file_path = question_obj["video_filename"]
+            video_index = question_obj["video_index"]
+            videos[str(video_index)].append({"question": question,
+                                             "answer": str(answer),
+                                             "template_filename": template_filename,
+                                             "video_file_path": video_file_path})
+    return videos
+
+
+def answer_counts(dataset_json) -> dict:
+    templates = dataset_grouped_by_templates(dataset_json)
+
+    answer_counts = defaultdict(int)
+
+    for template_filename in templates:
+        template_obj = templates[template_filename]
+        for question in template_obj:
+            answer_counts[str(question["answer"])] += 1
+
+    return answer_counts
+
+
 def balance_dataset(dataset_json) -> dict:
     new_dataset = []
     for qa_json in dataset_json:

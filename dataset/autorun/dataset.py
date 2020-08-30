e59b17c3a65ca64c125820a501e51a7fa1f01c87
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-08-30 22:15:24 +0300

    Add various statistics generator functions

--- dataset/autorun/dataset.py
+++ dataset/autorun/dataset.py
@@ -103,3 +103,125 @@ class SVQADataset:
         logging.info(f"Generating statistics: Answer frequencies in the dataset")
         stats.generate_stat__answer_frequencies()
 
+
+class DatasetStatistics:
+    def __init__(self, dataset: SVQADataset, output_folder="statistics", export_png=True):
+        self.dataset = SVQADataset(dataset.dataset_json_path, dataset.metadata_json_path)
+        self.export_png = export_png
+        self.output_folder = output_folder
+
+    def generate_pie_chart(self, title, counts, labels, colors, explodes):
+        fig1, ax1 = plt.subplots(figsize=(12, 12))
+        ax1.pie(counts,
+                labels=labels,
+                colors=colors,
+                autopct='%1.1f%%',
+                startangle=90,
+                radius=4,
+                explode=explodes)
+
+        """ax1.legend(wedges, answers_sorted,
+                  title="Answers",
+                  loc="best")"""
+
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        ax1.set_title(title)
+        plt.tight_layout()
+
+        if self.export_png:
+            if not os.path.exists(self.output_folder):
+                os.makedirs(self.output_folder)
+            plt.savefig(self.output_folder + os.path.sep + title + ".png")
+        else:
+            plt.show()
+
+    @staticmethod
+    def answer_counts_from_question_list(question_list: list) -> dict:
+        answer_counts = defaultdict(int)
+
+        for question in question_list:
+            answer_counts[str(question["answer"])] += 1
+
+        return answer_counts
+
+    @staticmethod
+    def counts_from_question_list(question_list: list, column: str) -> dict:
+        counts = defaultdict(int)
+
+        for question in question_list:
+            counts[str(question[column])] += 1
+
+        return counts
+
+    def generate_stat__answer_counts(self, answer_counts, title):
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        answers = list(answer_counts.keys())
+
+        colors = []
+        answers_sorted = []
+        counting_answers = [answer for answer in answers if self.dataset.get_answer_type_for_answer(answer) == "Count"]
+        shape_answers = [answer for answer in answers if self.dataset.get_answer_type_for_answer(answer) == "Shape"]
+        color_answers = [answer for answer in answers if self.dataset.get_answer_type_for_answer(answer) == "Color"]
+        boolean_answers = [answer for answer in answers if self.dataset.get_answer_type_for_answer(answer) == "Boolean"]
+
+        answers_sorted.extend(counting_answers)
+        if len(counting_answers) > 0:
+            colors.extend(Color("red").range_to(Color("#ffaaaa"), len(counting_answers)))
+        answers_sorted.extend(shape_answers)
+        if len(shape_answers) > 0:
+            colors.extend(Color("green").range_to(Color("#aaffaa"), len(shape_answers)))
+        answers_sorted.extend(color_answers)
+        if len(color_answers) > 0:
+            colors.extend(Color("blue").range_to(Color("#aaaaff"), len(color_answers)))
+        answers_sorted.extend(boolean_answers)
+        if len(boolean_answers) > 0:
+            colors.extend(Color("#555555").range_to(Color("#aaaaaa"), len(boolean_answers)))
+
+        counts = [answer_counts[answer] for answer in answers_sorted]
+
+        answers_sorted = [f"{answer} ({answer_counts[answer]})" for answer in answers_sorted]
+        # explodes_s = [0.1] * len(answers_sorted)
+        proportions = {c: c / sum(counts) for c in counts}
+        explodes = [pow(1 - proportions[c] / max(proportions.values()), 2) for c in counts]
+
+        colors_rgb = [color.rgb for color in colors]
+
+        self.generate_pie_chart(title, counts, labels=answers_sorted, colors=colors_rgb, explodes=explodes)
+
+    def generate_stat__answer_per_template(self):
+        answer_freq_v_template_id = []
+        template_ids = self.dataset.get_unique_values("template_id")
+        for tid in template_ids:
+            questions = Funnel(self.dataset.questions) \
+                .filter(lambda q: q["template_id"] == tid) \
+                .get_result()
+
+            answer_counts = DatasetStatistics.counts_from_question_list(questions, "answer")
+            self.generate_stat__answer_counts(answer_counts, f'Template ID={tid} - "{questions[0]["question"]}"')
+            for answer, count in answer_counts.items():
+                answer_freq_v_template_id.append({"template_id": tid, "answer": answer, "count": count})
+        df = pd.DataFrame(answer_freq_v_template_id)
+        write_to_file(f"{self.output_folder}{os.path.sep}Answer frequencies per each template ID.csv", df.to_csv())
+
+    def generate_stat__template_per_sim_id(self):
+        sim_id_v_template_freq = []
+        sim_ids = self.dataset.get_unique_values("simulation_id")
+        for sid in sim_ids:
+            questions = Funnel(self.dataset.questions) \
+                .filter(lambda q: q["simulation_id"] == sid) \
+                .get_result()
+
+            template_id_counts = DatasetStatistics.counts_from_question_list(questions, "template_id")
+            self.generate_stat__answer_counts(template_id_counts, f'Template ID frequencies for Simulation ID={sid}')
+            for tid, count in template_id_counts.items():
+                sim_id_v_template_freq.append({"simulation_id": sid, "template_id": tid, "count": count})
+
+        df = pd.DataFrame(sim_id_v_template_freq)
+        write_to_file(f"{self.output_folder}{os.path.sep}Template ID frequencies for each simulation type.csv",
+                      df.to_csv())
+
+    def generate_stat__answer_frequencies(self):
+        answer_counts = DatasetStatistics.counts_from_question_list(self.dataset.questions, "answer")
+        self.generate_stat__answer_counts(answer_counts, f'Answer frequencies (Total: {sum(answer_counts.values())})')
+
+

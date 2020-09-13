ce989ba6e8780164b72ecbdf4d05b62e1c27f294
cagatayyigit <cagatayyigit3@gmail.com>
2020-09-13 16:02:36 +0300

    Set opacity

--- dataset/autorun/object_segmentation.py
+++ dataset/autorun/object_segmentation.py
@@ -18,7 +18,10 @@ def read_old_snapshots(folder_path: str):
         sim_id = (file_name[:-5]).split("_")[0]       # simulation_id = 5
         frame = (file_name[:-5]).split("_")[1]        # current_frame = 375
 
+        if file_name == ".DS_Store":
+            continue
         with open(full_path) as json_file:
+
             d = dict()
             d["frame"] = frame
             d["simulation_id"] = sim_id
@@ -26,19 +29,48 @@ def read_old_snapshots(folder_path: str):
             all_snapshots.append(d)
     return all_snapshots
 
-## STEP 2
 
+## STEP 2
+"""
+"direction :
+"objects": []
+"""
 def create_all_combinations_from_one_snapshot(snapshot: dict) -> list:
     shape_list = ["cube", "circle", "triangle"]
     result = []
     for obj in snapshot["objects"]:
-        if obj["shape"] in shape_list and obj["size"]   == "large":
+        if obj["shape"] in shape_list:
             temp = dict()
             temp["directions"] = snapshot["directions"]
             temp["objects"] = [obj]
             result.append(temp)
 
     return result
+
+
+def get_static_objects(old_json_folder_path,new_json_folder_path):
+    p = old_json_folder_path + "/" + os.listdir(old_json_folder_path)[0]
+
+    file = open(p , "r")
+    snapshot = json.loads(file.read())
+    file.close()
+
+    shape_list = ["cube", "circle", "triangle"]
+
+    temp = dict()
+    temp["directions"] = snapshot["directions"]
+    temp["objects"] = []
+    for obj in snapshot["objects"]:
+        if obj["shape"] not in shape_list:
+            temp["objects"].append(obj)
+
+
+    f = open(new_json_folder_path + "/idstatic.json" , "w")
+    f.write(json.dumps(temp, indent=4))
+    f.close()
+    return temp
+
+
 
 def create_all_combinations(all_old_snapshots_dict: list):
     result = []
@@ -67,11 +99,13 @@ def write_new_snapshots_to_file(old_snapshots_folder_path: str, new_snapshots_fo
     for snapshot in arr:
         shape = snapshot["snapshot"]["objects"][0]["shape"]
         size = snapshot["snapshot"]["objects"][0]["size"]
-        name = "id" + snapshot["simulation_id"] + "_" + "frame" + snapshot["frame"] + "_" + size + "_" + shape
+        color = snapshot["snapshot"]["objects"][0]["color"]
+        name = "id" + snapshot["simulation_id"] + "_" + "frame" + snapshot["frame"] + "_" + size + "_" +color + "_" + shape
         f = open(new_snapshots_folder_path + "/" + name + ".json", "w")
         f.write(json.dumps(snapshot["snapshot"],indent=4))
         f.close()
 
+    get_static_objects(old_snapshots_folder_path, new_snapshots_folder_path)
 
 
 
@@ -95,10 +129,16 @@ def write_new_controller_to_file(new_snapshots: str, output_folder: str):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
+    sim_id = int(new_controllers[0].split("_")[0][2:])
     for f in new_controllers:
         if f == ".DS_Store":
             continue
-        j = get_controller_json(new_snapshots, f, int(f.split("_")[0][2:]) )
+        if f == "static.json":
+            j = get_controller_json(new_snapshots, "id_"+ f, sim_id)
+            file = open(output_folder + "/" + f, "w")
+            file.write(json.dumps(j, indent=4))
+            file.close()
+        j = get_controller_json(new_snapshots, f, sim_id )
         file = open(output_folder + "/" + f , "w")
         file.write(json.dumps(j, indent=4))
         file.close()
@@ -113,7 +153,7 @@ def run(controller_base_path: str, exe_path: str):
 
     for f in files:
         full_controller_path = controller_base_path + "/" +f
-        print("RUNNING SIMULATION::" + full_controller_path)
+        #print("RUNNING SIMULATION::" + full_controller_path)
         run_simulation(exe_path, full_controller_path)
 
 
@@ -131,38 +171,88 @@ new_controllers_folder = "/Users/cagatayyigit/Desktop/new_controllers"
 exec_path = "/Users/cagatayyigit/Projects/SVQA-Box2D/Build/bin/x86_64/Release/Testbed"
 
 
-#x(old_snapshots_folder, new_snapshots_folder,new_controllers_folder , exec_path)
-
+#x(old_snapshots_folder, new_snapshots_folder,new_controllers_folder, exec_path)
 
 
 from PIL import Image
 
+
+def make_transparent(img):
+    datas = img.getdata()
+    newData = []
+    for item in datas:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
+            newData.append((255, 255, 255, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    return img
+
+def get_transparent_image(image_path):
+    return make_transparent(Image.open(image_path).convert("RGBA"))
+
+
+
 img_base_path  = "/Users/cagatayyigit/Desktop/screenshots/"
-img1 = Image.open(img_base_path + "id5_frame140_large_circle.png")
-img2 = Image.open(img_base_path + "id5_frame510_large_circle.png")
 
 
-
-
-
-img = Image.blend(img1,img2, 0.9)
-
-#img.show()
-
-
-def abc():
-    result = Image.blend(img1, img2, 0.5)
-
-    files = os.listdir(img_base_path)
-    i = 0.0
+def get_all_image(base_path):
+    result = []
+    files = os.listdir(base_path)
+    filenames = []
+    sim_id = ""
+    frames = dict()
+    shape = dict()
     for f in files:
         if f == ".DS_Store":
             continue
+        if f == "idstatic.png":
+            continue
+        sim_id = str(f.split("_")[0])
+        frm = str(f.split("_")[1][5:])
 
-        new_img = Image.open(img_base_path + f)
-        result = Image.blend(new_img, result, 0.5)
+        shp = str(f.split("_")[2]) + "_" + str(f.split("_")[3]) + "_" + str(f.split("_")[4][:-4])
+
+        if frm not in frames:
+            frames[frm] = 1
+        if shp not in shape:
+            shape[shp] = 1
+    frame_arr = []
+    for k in frames.keys():
+        frame_arr.append(int(k))
+    frame_arr.sort()
+
+    shape_arr = []
+    for k in shape.keys():
+        shape_arr.append((k))
+
+    count = 1
+    total = len(shape_arr) * len(frame_arr)
+    for shape in shape_arr:
+        for frame in frame_arr: # id5_frame10_large_cyan_circle.png"
+            full_path = base_path + str(sim_id) + "_frame" + str(frame) + "_" + str(shape) + ".png"
+            print("Loaded:"+str(count) + "/" + str(total) + "  -->>" + full_path)
+            count += 1
+            result.append([get_transparent_image(full_path), frame])
+
+    return result
+
+def combine():
+    m1 = get_transparent_image("/Users/cagatayyigit/Desktop/screenshots/idstatic.png")
+    images = get_all_image("/Users/cagatayyigit/Desktop/screenshots/")
+    i = int(255 / 20)
+    j = 1
+    for img in images:
+        img[0].putalpha(i * (j + 10))
+        img[0] = make_transparent(img[0])
+        m1.paste(img[0], (0,0), img[0])
+        if j < 20:
+            j += 1
+        else:
+            j = 1
+    m1.show()
+    m1.save("/Users/cagatayyigit/Desktop/result.png")
 
 
-    result.show()
-
-
+combine()

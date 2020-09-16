721893bed2378456312e8c2946eef1bcfc6c4785
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-09-16 19:27:58 +0300

    Move dataset inspector from dataset_balancer.py script

--- dataset/autorun/dataset_balancer.py
+++ dataset/autorun/dataset_balancer.py
@@ -1,21 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 from autorun.dataset import SVQADataset
-from autorun.dataset_statistics import DatasetStatistics
-
-
-class DatasetInspector:
-
-    def __init__(self, stats: DatasetStatistics):
-        self.stats = stats
-        self.dataset = stats.dataset
-
-    def inspect_tid_sid_v_answer_balance(self):
-        for tid, sid, answer, answer_type, count in self.stats.answer_freq_per_tid_and_sid.items():
-            pass
-
-
 
 
 class DatasetBalancer:

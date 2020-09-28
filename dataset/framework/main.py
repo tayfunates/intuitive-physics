5f0ff473a16f8b9ddb8f308d7b91c868eb17e869
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2020-09-28 19:56:26 +0300

    Add example dataset generation script to main.py

--- dataset/framework/main.py
+++ dataset/framework/main.py
@@ -1,38 +1,16 @@
-import json
-import time
+import sys
 
 from loguru import logger
 
-from framework.balance import DatasetInspector, DatasetBalancer, DatasetUnderSampler
+from framework.dataset import DatasetGenerationConfig, DatasetGenerator
 from framework.utils import FileIO
-from framework.dataset import DatasetGenerationConfig, SVQADataset, DatasetStatistics, DatasetUtils
 
 if __name__ == '__main__':
+    # Enqueue is because of multiprocessing.
+    logger.add("out/dataset_generation.log", enqueue=True)
 
-   #dataset_json = FileIO.read_json("C:/shamil/stuff/Dataset_3000_270820/dataset.json")
-   #dataset_as_list = DatasetUtils.dataset_as_list(dataset_json, "../svqa/metadata.json")
-   #FileIO.write_json(dataset_as_list, "C:/shamil/stuff/Dataset_3000_270820/dataset_as_list.json")
+    dataset_generation_config_file_path = sys.argv[1]
+    config = DatasetGenerationConfig(FileIO.read_json(dataset_generation_config_file_path))
 
-   #dataset = SVQADataset("C:/shamil/stuff/Dataset_3000_270820", "../svqa/metadata.json")
-   #undersampler = DatasetUnderSampler(dataset, "C:/shamil/stuff/Dataset_3000_270820/dataset_as_list.json")
-   #undersampler.balance_answers_within_each_template_and_simulation_ids().dump_as_list()
-
-    logger.add(f"out/dataset_balancing_{time.time()}.log")
-
-
-
-    config = DatasetGenerationConfig(FileIO.read_json("C:/shamil/stuff/Dataset_3000_270820/dataset_gen_config.json"))
-
-    #dataset = SVQADataset("C:/shamil/stuff/Dataset_3000_270820", "../svqa/metadata.json")
-
-    #dataset_stats = DatasetStatistics(dataset)
-    #dataset_stats.generate_stat__answer_per_tid_and_sid()
-
-    #inspector = DatasetInspector(dataset_stats)
-
-    #answers_needed = inspector.inspect_tid_and_sid_versus_answer_balance()
-
-    dataset_balancer = DatasetBalancer(config, None, 3000)
-
-    dataset_balancer.start_balancing()
-
+    dataset_generator = DatasetGenerator(config)
+    dataset_generator.execute()

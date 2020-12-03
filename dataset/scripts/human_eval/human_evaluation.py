d014a3849c05239b53a02f5cc866d6bb85b8d736
cagatayyigit <cagatayyigit3@gmail.com>
2020-12-03 15:22:17 +0300

    Refactor

--- dataset/scripts/human_eval/human_evaluation.py
+++ dataset/scripts/human_eval/human_evaluation.py
@@ -1,43 +1,11 @@
-import json
 from framework.utils import FileIO
+import pandas as pd
 
 questions = FileIO.read_json("../human_eval/questions.json")
-
 def getQuestionInfo(questionText: str, video_index: int):
     for q in questions:
         if q["question"] == questionText and int(q["video_index"]) == video_index:
             return q
-
-question_type = ["Prevent", "Counterfactual", "Descriptive", "Enable", "Cause"]
-template_id = []
-answer_type = ["Boolean", "Count", "Color", "Shape"]
-
-import pandas as pd
-
-
-def getResponses(csv_path):
-    data = pd.read_csv(csv_path)
-    res_matrix  = [[key for key in data.keys()]]
-    for i in range(len(data)):
-        temp = []
-        for user_response in data.iloc[i]:
-            temp.append(user_response)
-        res_matrix.append(temp)
-    res_matrix.pop(2)
-    return res_matrix
-
-
-part1_data = getResponses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part1.csv")
-part2_data = getResponses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part2.csv")
-part3_data = getResponses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part3.csv")
-part4_data = getResponses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part4.csv")
-part5_data = getResponses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part5.csv")
-
-
-
-
-
-data = pd.read_csv("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part1.csv")
 
 
 class Question:
@@ -67,34 +35,46 @@ class Question:
         print()
 
 
-user_datas = []
-
-for key in data.keys()[17:]:
-    question = Question(key, data[key][0])
-    user_datas.append(question)
-    ans_temp = []
-    for ans in data[key][2:]:
-        ans_temp.append(ans)
-    question.set_user_answers(ans_temp)
 
 
+def extract_user_responses(csv_path):
+    data = pd.read_csv(csv_path)
+    user_datas = []
+    for key in data.keys()[17:]:
+        question = Question(key, data[key][0])
+        user_datas.append(question)
+        ans_temp = []
+        for ans in data[key][2:]:
+            ans_temp.append(ans)
+        question.set_user_answers(ans_temp)
+
+    for q in user_datas:
+        q_number = int((q.q_number[1:]).split(".")[0])
+        q_text = q.q_text
+
+        info = getQuestionInfo(q_text, q_number)
+
+        if info:
+            q.set_actual_answer(info["answer"])
+            q.set_question_type(info["question_type"])
+        else:
+            q.set_actual_answer("not found")
+            q.set_question_type("not found")
+
+    return user_datas
 
 
-
-
-for q in user_datas:
-    q_number = int((q.q_number[1:]).split(".")[0])
-    q_text = q.q_text
-
-    info = getQuestionInfo(q_text, q_number)
-
-    if info:
-        q.set_actual_answer(info["answer"])
-        q.set_question_type(info["question_type"])
-    else:
-        q.set_actual_answer("not found")
-        q.set_question_type("not found")
-
-
-for i in user_datas:
+part1_data = extract_user_responses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part1.csv")
+for i in part1_data:
     i.printQuestion()
+
+part2_data = extract_user_responses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part2.csv")
+
+
+part3_data = extract_user_responses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part3.csv")
+
+
+part4_data = extract_user_responses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part4.csv")
+
+
+part5_data = extract_user_responses("/Users/cagatayyigit/Projects/intuitive-physics/dataset/scripts/human_eval/part5.csv")

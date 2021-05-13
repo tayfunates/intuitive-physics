e642396f6d21b2dc630393554a1c7d7ce3b1e55f
cagatayyigit <cagatayyigit3@gmail.com>
2021-05-13 17:47:01 +0300

    Update object_seg_statics

--- dataset/scripts/object_segmentation/object_segmentation_statics.py
+++ dataset/scripts/object_segmentation/object_segmentation_statics.py
@@ -2,6 +2,74 @@ import json
 import os
 import subprocess
 from PIL import Image
+from pathlib import Path
+
+
+
+
+def get_controller_json_for_statics(min_mean_max_random: str, simulation_id: int, screenshot_output_folder: str):
+    return json.loads(
+        f"""{{
+                "simulationID": {simulation_id},
+                "offline": true,
+                "outputVideoPath": "output.mpg",
+                "outputJSONPath":  "output.json",
+                "width": 1024,
+                "height": 1024,
+                "inputScenePath":  "",
+                "includeDynamicObjects": false,
+                "screenshotOutputFolder": "{screenshot_output_folder}",
+                "staticObjectPositioningType": "{min_mean_max_random}",
+                "stepCount": 3
+            }}""")
+
+
+def create_and_save_all_jsons_for_statics(num_of_scenes, screenshot_output_folder: str, new_jsons_output_folder: str):
+    res = []
+    for scene_id in range(1, num_of_scenes + 1):
+        min_ = get_controller_json_for_statics("min", scene_id, screenshot_output_folder)
+        mean_ = get_controller_json_for_statics("mean", scene_id, screenshot_output_folder)
+        max_ = get_controller_json_for_statics("max", scene_id, screenshot_output_folder)
+        res.append(min_)
+        res.append(mean_)
+        res.append(max_)
+
+        json_name = f"sid_{scene_id}_min.json"
+        file = open(new_jsons_output_folder + "/" + json_name, "w")
+        file.write(json.dumps(min_, indent=4))
+        file.close()
+
+        json_name = f"sid_{scene_id}_mean.json"
+        file = open(new_jsons_output_folder + "/" + json_name, "w")
+        file.write(json.dumps(mean_, indent=4))
+        file.close()
+
+        json_name = f"sid_{scene_id}_max.json"
+        file = open(new_jsons_output_folder + "/" + json_name, "w")
+        file.write(json.dumps(max_, indent=4))
+        file.close()
+
+    return res
+
+
+
+
+def run_simulation(exec_path: str, controller_json_path: str):
+    subprocess.call(f"{exec_path} {controller_json_path}", shell=True, universal_newlines=True)
+
+
+def run(controller_base_path: str, exe_path: str):
+    files = os.listdir(controller_base_path)
+
+    for f in files:
+        full_controller_path = controller_base_path + "/" + f
+        run_simulation(exe_path, full_controller_path)
+
+
+exec_path = Path("../../../simulation/2d/SVQA-Box2D/Build/bin/x86_64/Release/Testbed").absolute().as_posix()
+
+#create_and_save_all_jsons_for_statics(20,"C:/Users/cagatay/OneDrive/Desktop/static_ss","C:/Users/cagatay/OneDrive/Desktop/new_jsons")
+#run("C:/Users/cagatay/OneDrive/Desktop/new_jsons/", exec_path)
 
 
 def read_img_and_make_transparent(image_path):
@@ -34,56 +102,3 @@ def combine_statics(result_path, img1_path, img2_path, img3_path):
 
 d = "C:/Users/cagatay/OneDrive/Desktop/"
 # combine_statics(d + "result.png", d+"/static_ss/1.png", d+"/static_ss/2.png", d+"/static_ss/3.png")
-
-exe = "./../../intuitive-physics/simulation/2d/SVQA-Box2D/Build/bin/x86_64/Release/Testbed"
-
-
-def run_simulation(exec_path: str, controller_json_path: str):
-    subprocess.call(f"{exec_path} {controller_json_path}", shell=True, universal_newlines=True)
-
-
-def run(controller_base_path: str, exe_path: str):
-    files = os.listdir(controller_base_path)
-
-    for f in files:
-        full_controller_path = controller_base_path + "/" + f
-        run_simulation(exe_path, full_controller_path)
-
-
-def get_controller_json_for_statics(min_mean_max_random: str, simulation_id: int, screenshot_output_folder: str):
-    return json.loads(
-        f"""{{
-                "simulationID": {simulation_id},
-                "offline": true,
-                "outputVideoPath": "output.mpg",
-                "outputJSONPath":  "output.json",
-                "width": 1024,
-                "height": 1024,
-                "inputScenePath":  "",
-                "includeDynamicObjects": false,
-                "screenshotOutputFolder": "{screenshot_output_folder}",
-                "staticObjectOrientationType": "{min_mean_max_random}",
-                "stepCount": 3
-            }}""")
-
-
-def create_all_jsons_for_statics(num_of_scenes, screenshot_output_folder: str):
-    res = []
-    for scene_id in range(1, num_of_scenes + 1):
-        min_ = get_controller_json_for_statics("min", scene_id, screenshot_output_folder)
-        mean_ = get_controller_json_for_statics("mean", scene_id, screenshot_output_folder)
-        max_ = get_controller_json_for_statics("max", scene_id, screenshot_output_folder)
-        res.append(min_)
-        res.append(mean_)
-        res.append(max_)
-
-    return res
-
-
-arr = create_all_jsons_for_statics(20, "C:/Users/cagatay/OneDrive/Desktop/static_ss")
-
-
-
-
-for a in arr:
-    print(a)
